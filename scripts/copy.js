509ae499cc5b4a8d5c88b8993b614cba8ded61c2
ateals <ateals@icloud.com>
2024-05-22 05:15:31 +0900

    feat: mdx 파일 이름 해싱

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,6 +1,7 @@
 // copy.js
 const fs = require("fs");
 const path = require("path");
+const crypto = require("crypto");
 
 function copyFiles(options) {
   const { input, output, exclude = [] } = options;
@@ -17,12 +18,28 @@ function copyFiles(options) {
     }
 
     const srcFile = path.join(input, file);
-    const destFile = path.join(output, file);
+    let destFile = path.join(output, file);
 
     if (fs.lstatSync(srcFile).isDirectory()) {
       fs.mkdirSync(destFile, { recursive: true });
       copyFiles({ input: srcFile, output: destFile, exclude });
     } else {
+      if (path.extname(file) === ".md") {
+        const parsedPath = path.parse(destFile);
+        parsedPath.ext = ".mdx";
+        parsedPath.base = `${parsedPath.name}${parsedPath.ext}`;
+        destFile = path.format(parsedPath);
+
+        const hash = crypto.createHash("sha256");
+        hash.update(file);
+        const hashedName = hash.digest("hex");
+
+        const hashedPath = path.parse(destFile);
+        hashedPath.name = hashedName;
+        hashedPath.base = `${hashedName}${hashedPath.ext}`;
+        destFile = path.format(hashedPath);
+      }
+
       fs.copyFileSync(srcFile, destFile);
     }
   });
