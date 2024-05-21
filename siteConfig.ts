f6f703efdd09c316e939b79eeeb2493c152871a7
ateals <ateals@icloud.com>
2024-05-22 05:15:16 +0900

    feat: next-sitemap sitemap 생성

--- siteConfig.ts
+++ siteConfig.ts
@@ -1,6 +1,7 @@
 export default {
   URL: {
     BLOG: "https://ateals.vercel.app",
+    DOCS: "https://ateals-docs.vercel.app",
   },
   LOGO: "https://ateals.vercel.app/images/logo.webp",
   meta: {
