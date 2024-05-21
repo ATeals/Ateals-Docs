eebae443101ffa3358e8f30abf607dde279f75c3
ateals <ateals@icloud.com>
2024-05-22 05:44:09 +0900

    feat: add favicon

---
+++ docs/_document.jsx
@@ -0,0 +1,16 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function render() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <title>Ateals - docs</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
