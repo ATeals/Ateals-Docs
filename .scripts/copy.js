e65572c09939054803430eef65f610140a011130
ateals <ateals@icloud.com>
2024-05-25 22:13:26 +0900

    feat: copy.js class 모듈화

--- .scripts/copy.js
+++ .scripts/copy.js
@@ -1,48 +1,96 @@
-// copy.js
 import { existsSync, rmSync, mkdirSync, readdirSync, lstatSync, copyFileSync } from "fs";
 import { join, extname, parse, format } from "path";
 import { createHash } from "crypto";
 
-function copyFiles(options) {
-  const { input, output, exclude = [] } = options;
-
-  if (existsSync(output)) {
-    rmSync(output, { recursive: true, force: true });
+class Utils {
+  static convertToMdx(destFile) {
+    const parsedPath = parse(destFile);
+    parsedPath.ext = ".mdx";
+    parsedPath.base = `${parsedPath.name}${parsedPath.ext}`;
+    return format(parsedPath);
   }
 
-  mkdirSync(output);
-
-  readdirSync(input).forEach((file) => {
-    if (exclude.includes(file)) {
-      return;
-    }
-
-    const srcFile = join(input, file);
-    let destFile = join(output, file);
-
-    if (lstatSync(srcFile).isDirectory()) {
-      mkdirSync(destFile, { recursive: true });
-      copyFiles({ input: srcFile, output: destFile, exclude });
-    } else {
-      if (extname(file) === ".md") {
-        const parsedPath = parse(destFile);
-        parsedPath.ext = ".mdx";
-        parsedPath.base = `${parsedPath.name}${parsedPath.ext}`;
-        destFile = format(parsedPath);
-
-        const hash = createHash("sha256");
-        hash.update(file);
-        const hashedName = hash.digest("hex");
-
-        const hashedPath = parse(destFile);
-        hashedPath.name = hashedName;
-        hashedPath.base = `${hashedName}${hashedPath.ext}`;
-        destFile = format(hashedPath);
-      }
-
-      copyFileSync(srcFile, destFile);
-    }
-  });
+  static hashFileName(destFile) {
+    const parsedPath = parse(destFile);
+    const hash = createHash("sha256");
+    hash.update(parsedPath.base);
+    const hashedName = hash.digest("hex");
+    parsedPath.name = hashedName;
+    parsedPath.base = `${hashedName}${parsedPath.ext}`;
+    return format(parsedPath);
+  }
 }
 
-copyFiles({ input: "./docs", output: "./pages", exclude: [".obsidian", "Tamplate"] });
+class DocsManager {
+  constructor(options) {
+    const { input, output, exclude = [] } = options;
+    this.input = input;
+    this.output = output;
+    this.exclude = exclude;
+  }
+
+  cleanOutput() {
+    if (existsSync(this.output)) {
+      rmSync(this.output, { recursive: true, force: true });
+    }
+    mkdirSync(this.output);
+
+    return this;
+  }
+
+  isFolder(file) {
+    return lstatSync(join(this.input, file)).isDirectory();
+  }
+
+  copyFiles({
+    fileEvents,
+    options: { input = this.input, output = this.output, exclude = this.exclude } = {},
+  }) {
+    readdirSync(input).forEach((file) => {
+      if (exclude.includes(file)) {
+        return;
+      }
+
+      const srcFile = join(input, file);
+
+      let destFile = join(output, file);
+
+      if (this.isFolder(file)) {
+        mkdirSync(destFile, { recursive: true });
+        new DocsManager({ input: srcFile, output: destFile, exclude: exclude }).copyFiles({
+          fileEvents,
+          options: { input: srcFile, output: destFile, exclude },
+        });
+      } else {
+        if (fileEvents) {
+          for (const { fileExt, handler } of fileEvents) {
+            if (extname(file) === `.${fileExt}`) {
+              destFile = handler(destFile, this);
+            }
+          }
+        }
+        copyFileSync(srcFile, destFile);
+      }
+    });
+
+    return this;
+  }
+}
+
+const manager = new DocsManager({
+  input: "./docs",
+  output: "./pages",
+  exclude: [".obsidian", "Tamplate"],
+});
+
+manager.cleanOutput().copyFiles({
+  fileEvents: [
+    {
+      fileExt: "md",
+      handler: (destFile) => {
+        destFile = Utils.convertToMdx(destFile);
+        return Utils.hashFileName(destFile);
+      },
+    },
+  ],
+});
